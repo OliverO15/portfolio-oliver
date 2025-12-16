75bb055fd5dae7a2d87eda9472d84427356a0b34
Oliver O <oliverormar5@gmail.com>
2025-12-16 15:14:56 +0000

    setup in Vite

---
+++ portfolio-app/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Navbar from "./components/navbar/navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <h1>Oliver Ormar</h1>
+    </>
+  );
+}
+
+export default App;
